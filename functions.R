cf1ecf3b779c1206c72256987f9b0af1c08e0c9b
gast-dan <danielgast91@gmail.com>
2022-01-20 20:04:38 +0100

    Grundlage für 2. Aufgabe
    Leere .R Dateien für die Funktionen

---
+++ functions.R
@@ -0,0 +1,68 @@
+
+# Eine Funktion, die verschiedene geeignete deskriptive Statistiken für metrische Variablen berechnet und ausgibt
+# Name ist Platzhalter!
+function_a = function(){
+  
+  
+  
+  
+}
+
+
+
+#Eine Funktion, die verschiedene geeignete deskriptive Statistiken für kategoriale Variablen berechnet und ausgibt
+# Name ist Platzhalter!
+function_b = function(){
+  
+  
+  
+  
+}
+
+#Eine Funktion, die geeignete deskriptive bivariate Statistiken für den Zusammenhang zwischen zwei kategorialen Variablen berechnet ausgibt
+# Name ist Platzhalter!
+function_c = function(){
+  
+  
+  
+  
+}
+
+#Eine Funktion, die geeignete deskriptive bivariate Statistiken für den Zusammengang zwischen einer metrischen und einer dichotomen Variablen berechnet und ausgibt
+# Name ist Platzhalter!
+function_d = function(){
+  
+  
+  
+  
+}
+
+#Eine Funktion, die eine mindestens ordinal skalierte Variable quantilbasiert kategorisiert (z.B. in „niedrig“, „mittel“, „hoch“)
+# Name ist Platzhalter!
+function_e = function(){
+  
+  
+  
+  
+}
+
+
+#Eine Funktion, die eine geeignete Visualisierung von drei oder vier kategorialen Variablen erstellt
+# Name ist Platzhalter!
+function_f = function(){
+  
+  
+  
+  
+}
+
+
+
+#Freiwillig: weitere zur Deskription und Visualisierung geeignete Funktionen
+# Name ist Platzhalter!
+function_g = function(){
+  
+  
+  
+  
+}
