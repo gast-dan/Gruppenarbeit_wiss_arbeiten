07bdcde06d6a2c990f969758c0eee4cedf02d5f8
liianaa <94876040+liianaa@users.noreply.github.com>
2022-02-28 19:05:34 +0100

    Update helper.R

--- helper.R
+++ helper.R
@@ -2,9 +2,9 @@
 categories = function(data, quantiles){
   
   new_data = c()
-  new_data[data < quantiles[[2]]] = "niedrig"
-  new_data[data >= quantiles[[2]] & data <= quantiles[[length(quantiles)-2]]] = "mittel"
-  new_data[data > quantiles[[length(quantiles)-2]]] = "hoch"
+  new_data[data <= quantiles[[2]]] = "niedrig"
+  new_data[data > quantiles[[2]] & data < quantiles[[length(quantiles)-1]]] = "mittel"
+  new_data[data >= quantiles[[length(quantiles)-1]]] = "hoch"
   
   
   return(new_data)
